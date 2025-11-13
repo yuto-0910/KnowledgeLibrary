e4119b07038f7106644b0314a6a6d5de7b4ae8e0
yuto-0910 <104240868+yuto-0910@users.noreply.github.com>
2025-11-13 15:16:05 +0900

    play let!

---
+++ JS_Standard/my-prac/let.js
@@ -0,0 +1,17 @@
+let x;
+x=12;
+if(x===10){
+   x = 13;
+}
+console.log(x);
+
+let msg="hello!";
+msg="good!";
+console.log(msg);
+
+if(x===12 && msg === "good!"){
+  console.log(msg + "x" + x);
+}else{
+console.log("false");
+}
+
